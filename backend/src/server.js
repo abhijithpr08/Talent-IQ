4bdef8d5a6868135ecaf749476b493a1fd1c569a
abhijithpr08 <abhith628@gmail.com>
2026-02-03 09:56:35 +0530

    start to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
-import express from "express"
+import express from "express";
+import path from "path";
 import { ENV } from "./lib/env.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
+const __dirname = path.resolve()
+
+app.get("/hey", (req, res) => {
     res.status(200).json({ msg: "success" })
 });
+
+//make our app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("/{*any}", (req,res)=> {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+}
 
 app.listen(ENV.PORT, () => 
     console.log(`server is running on post http://localhost:${ENV.PORT}`
