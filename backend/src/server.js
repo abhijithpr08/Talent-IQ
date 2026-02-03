ea274d90ffb867f76d10ebfc72e60c60e70fa4a8
abhijithpr08 <abhith628@gmail.com>
2026-02-03 10:55:12 +0530

    move vite to dependencies for render build

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,21 +4,23 @@ import { ENV } from "./lib/env.js";
 
 const app = express();
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-app.get("/hey", (req, res) => {
-    res.status(200).json({ msg: "success" })
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-//make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "this is the books endpoint" });
+});
 
-    app.get("/{*any}", (req,res)=> {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    })
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(ENV.PORT, () => 
-    console.log(`server is running on post http://localhost:${ENV.PORT}`
-));
+app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
