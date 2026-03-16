34bba890b3f156a92e1f690222cb2367a7d4e130
abhijithpr08 <abhith628@gmail.com>
2026-03-16 12:51:41 +0530

    fix express catch all route

--- backend/src/server.js
+++ backend/src/server.js
@@ -43,7 +43,7 @@ if (ENV.NODE_ENV === "production") {
 
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/:path(*)", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
